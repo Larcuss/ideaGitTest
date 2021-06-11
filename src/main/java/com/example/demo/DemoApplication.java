ac24915041746f8aeef18e54c3e2a08d1c644dcf
Larcuss <xxksharp@foxmail.com>
2021-06-11 09:01:24 +0800

    springboot test1

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+//+LOMBOK(in spring initializr) +devtool
+//.idea // day4-sb.iml 不可提交
+//static=CSS JS IMG templates=webpage
+//yml -> properties
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
