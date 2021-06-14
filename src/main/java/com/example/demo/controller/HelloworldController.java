f216b5784f74280283b31b4b30360a85cb13ac26
Larcuss <xxksharp@foxmail.com>
2021-06-14 20:37:56 +0800

    springboot +controller[real]

--- src/main/java/com/example/demo/controller/HelloworldController.java
+++ src/main/java/com/example/demo/controller/HelloworldController.java
@@ -9,10 +9,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.Map;
 
 //lombok @Data标识bean(getter+setter,不用自己写) @Accessors链式写法 bean.set.set.set(连续) @Repository标识bean.java
 //持久层框架mybatis +plus便捷
-//
+//+node.js npm=pip cnpm/npm一个项目中只用一个
+//v-if
+
 @Controller
 public class HelloworldController {
     @Autowired
@@ -20,11 +24,20 @@ public class HelloworldController {
 
     @ResponseBody
     @RequestMapping("/h")//necessary json-map返回map
-    public User hello(User u)
+    public Map hello(String req)
     {
-        user.setName(u.getName());
-        user.setAge(u.getAge());
-        return user;
+        Map<String, Object> map = new HashMap<>();
+        System.out.println(req);
+        char t = req.charAt(0);
+        if(req != null && t == 'I')
+        {
+            map.put("code", 200);
+        }
+        else
+        {
+            map.put("code", -1);
+        }
+        return map;
     }
 
 }
